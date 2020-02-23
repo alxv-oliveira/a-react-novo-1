0856cccfbfe68f77dd20f0a885d8023319a90b29
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-23 17:14:53 -0300

    Novo component e estilo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
 
+import Header from './Header';
 import Tabela from './Tabela';
 import Form from './Formulario';
 
@@ -47,8 +49,11 @@ class App extends Component {
     render() {
         return(
             <Fragment>
-                <Tabela autores={this.state.autores} removeAutor={this.removeAutor}/>
-                <Form escutadorDeSubmit={this.escutadorDeSubmit}/>
+                <Header />
+                <div className="container mb-10">
+                    <Tabela autores={this.state.autores} removeAutor={this.removeAutor}/>
+                    <Form escutadorDeSubmit={this.escutadorDeSubmit}/>
+                </div>
             </Fragment>
         );
     }
--- src/Tabela.js
+++ src/Tabela.js
@@ -20,7 +20,7 @@ const TableBody = (props) => {
                 <td>{linha.nome}</td>
                 <td>{linha.livro}</td>
                 <td>{linha.preco}</td>
-                <td><button onClick={() => { props.removeAutor(index) }}>Remover</button></td>
+                <td><button onClick={() => { props.removeAutor(index) }} className="waves-effect waves-light red lighten-1 btn">Remover</button></td>
             </tr>
         );
     });
@@ -37,7 +37,7 @@ class Tabela extends Component {
         const { autores, removeAutor } = this.props;
 
         return (
-            <table>
+            <table className="centered highlight">
                 <TableHead />
                 <TableBody autores={autores} removeAutor={removeAutor}/>
             </table>
